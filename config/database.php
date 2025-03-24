e6abf8a93c17f33475545c8dc24591fa22fcbb75
Chase Mako <makochased@gmail.com>
2025-03-24 15:50:00 +0000

    Add files via upload

--- config/database.php
+++ config/database.php
@@ -1,1 +1,18 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="userdata";
 
+
+
+$conn=new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connect_error){
+    die("<p style='color: red;text-align: center;'>Connection failed to establish".$conn->connect_error."</p>");
+}else{
+    echo "<p id='message' style='color: green; text-align: center;'>Connected successfully</p>";
+}
+?>
+
+<script src="/assets/js/timer.js"></script>
